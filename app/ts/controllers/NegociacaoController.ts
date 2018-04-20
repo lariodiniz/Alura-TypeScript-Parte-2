fda5ce361ad297952a8d58bb1474e3879fb9d02b
LarioDiniz <lario.diniz@palo.com.br>
2018-04-20 09:38:53 -0300

    Aula 02
    Aula 02

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,7 +1,8 @@
-import { MensagemView, NegociacoesView } from '../views/index'
-import { Negociacao, Negociacoes } from '../models/index'
+import { NegociacoesView, MensagemView } from '../views/index';
+import { Negociacao, Negociacoes } from '../models/index';
 
-export class NegociacaoController{
+export class NegociacaoController {
+
     private _inputData: JQuery;
     private _inputQuantidade: JQuery;
     private _inputValor: JQuery;
@@ -9,24 +10,50 @@ export class NegociacaoController{
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
 
-    constructor(){
-        this._inputData =$('#data');
-        this._inputQuantidade =$('#quantidade');
+    constructor() {
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._negociacoesView.update( this._negociacoes);
+        this._negociacoesView.update(this._negociacoes);
     }
 
-    adiciona(event: Event){
+    adiciona(event: Event) {
 
         event.preventDefault();
 
-        let  negociacao = new Negociacao(
-            new Date(this._inputData.val().replace(/-/g,'/')),
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if(!this._ehDiaUtil(data)) {
+
+            this._mensagemView.update('Somente negociações em dias úteis, por favor!');
+            return 
+        }
+
+        const negociacao = new Negociacao(
+            data, 
             parseInt(this._inputQuantidade.val()),
-            parseFloat(this._inputValor.val()),
+            parseFloat(this._inputValor.val())
         );
+
         this._negociacoes.adiciona(negociacao);
-        this._negociacoesView.update( this._negociacoes);
-        this._mensagemView.update('Negociações adicionadas com sucesso!');
+
+        this._negociacoesView.update(this._negociacoes);
+        this._mensagemView.update('Negociação adicionada com sucesso!');
     }
+
+    private _ehDiaUtil(data: Date) {
+
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana {
+
+    Domingo, 
+    Segunda, 
+    Terca, 
+    Quarta, 
+    Quinta, 
+    Sexta, 
+    Sabado
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,13 +1,20 @@
 
 export abstract class View<T> {
     protected _elemento: JQuery;
+    protected _escapar : boolean;
 
-    constructor(selector: string){
+    constructor(selector: string, escapar: boolean = false){
         this._elemento = $(selector);
+        this._escapar = escapar;
     }
 
     update(modal:T): void{
-        this._elemento.html(this.template(modal));
+        
+        let template = this.template(modal);
+
+        if (this._escapar)
+        template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        this._elemento.html(template);
     }
 
     abstract template(modal:T): string;
